6ad3c6302874c28fa2e3d9ebe91ef9f878335bc8
abimanyukali <abiabimanyu350@gmail.com>
2026-03-01 15:38:15 +0530

    turn

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,7 +4,7 @@ export const ICE_CONFIG = {
       urls: 'stun:stun.l.google.com:19302',
     },
     {
-      urls: 'turn:abimanyuresearchlab.online:3478',
+      urls: 'turn:100.25.96.44:3478',
       username: 'user',
       credential: 'password',
     },
@@ -13,4 +13,4 @@ export const ICE_CONFIG = {
 
 export const SIGNAL_SERVER_URL =
   process.env.NEXT_PUBLIC_SIGNAL_SERVER_URL ||
-  'https://abimanyuresearchlab.online';
+  'https://abimanyuresearchlab.online' 
