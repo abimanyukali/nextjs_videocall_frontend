f8878401abaf1b1f117c926ccc8a74f6c30cb879
abimanyukali <abiabimanyu350@gmail.com>
2026-03-01 15:40:27 +0530

    nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  
   allowedDevOrigins: [
     'http://localhost:3000',
     'https://localhost:3000',
@@ -11,10 +9,12 @@ const nextConfig = {
     'http://192.168.1.33:5000',
     'https://686dffq3-3000.inc1.devtunnels.ms',
     'https://686dffq3-5000.inc1.devtunnels.ms',
-    "http://54.242.142.48:3000",
-    "http://54.242.142.48:5000",
-    "https://54.242.142.48:3000",
-    "https://54.242.142.48:5000",
+    'http://54.242.142.48:3000',
+    'http://54.242.142.48:5000',
+    'https://54.242.142.48:3000',
+    'https://54.242.142.48:5000',
+    'https://abimanyuresearchlab.online',
+    'https://nextjs-videocall-frontend.vercel.app',
   ],
 };
 
